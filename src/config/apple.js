fbaaacc37412facc1d6bf84d2c2cc4fe67684b33
michaljarnot <michal.jarnot@strv.com>
2018-01-07 19:17:40 +0100

    Set apple's BLUR to 17
    - Hitting apple makes it fall into many pieces we need to ignore those pieces

--- src/config/apple.js
+++ src/config/apple.js
@@ -3,7 +3,7 @@ import { Vec } from 'opencv4nodejs'
 
 export const UPPER_COLOR = new Vec(200, 65, 255)
 export const LOWER_COLOR = new Vec(10, 10, 130)
-export const BLUR = 5
+export const BLUR = 17
 export const OFFSET = -50
 export const REGION_SIZE = 100
 export const PATH = './src/images/withAppleSmall.jpg'
